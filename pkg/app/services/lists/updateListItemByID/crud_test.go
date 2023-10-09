e46662d272ca2f1459c1f3c387aff7bb7d652947
Mario <marioskrlec222@gmail.com>
2023-10-09 20:41:15 +0200

    Created service to update a list item

---
+++ pkg/app/services/lists/updateListItemByID/crud_test.go
@@ -0,0 +1,99 @@
+package updateListItemByID
+
+import (
+	"creatif/pkg/app/domain/declarations"
+	"creatif/pkg/lib/storage"
+	"encoding/json"
+	"fmt"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+)
+
+var _ = ginkgo.Describe("Declaration (UPDATE) variable tests", func() {
+	ginkgo.It("should update the name of the list item variable", func() {
+		projectId := testCreateProject("project")
+		testCreateList(projectId, "name", 100)
+
+		var singleItem declarations.ListVariable
+		storage.Gorm().Raw(
+			fmt.Sprintf("SELECT lv.id AS id FROM %s AS lv INNER JOIN %s AS l ON lv.list_id = l.id AND l.name = ? AND l.project_id = ?", (declarations.ListVariable{}).TableName(), (declarations.List{}).TableName()),
+			"name",
+			projectId,
+		).Scan(&singleItem)
+
+		m := "text value"
+		v, err := json.Marshal(m)
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		handler := New(NewModel(projectId, []string{"name", "behaviour"}, "name", singleItem.ID, "newName", "readonly", []string{}, []byte{}, v))
+
+		updated, err := handler.Handle()
+		testAssertErrNil(err)
+		testAssertIDValid(updated.ID)
+
+		gomega.Expect(updated.Name).Should(gomega.Equal("newName"))
+
+		var checkModel declarations.ListVariable
+		res := storage.Gorm().Table(checkModel.TableName()).Where("id = ?", updated.ID).First(&checkModel)
+		testAssertErrNil(res.Error)
+
+		gomega.Expect(checkModel.Name).Should(gomega.Equal("newName"))
+		gomega.Expect(checkModel.Behaviour).Should(gomega.Equal("readonly"))
+	})
+
+	ginkgo.It("should update the groups of the declaration variable", func() {
+		ginkgo.Skip("")
+		projectId := testCreateProject("project")
+		testCreateList(projectId, "name", 100)
+
+		m := "text value"
+		v, err := json.Marshal(m)
+		gomega.Expect(err).Should(gomega.BeNil())
+		handler := New(NewModel(projectId, []string{"name", "groups", "value"}, "name", "newName", "newName", "readonly", []string{"first", "second", "third"}, []byte{}, v))
+
+		updated, err := handler.Handle()
+		testAssertErrNil(err)
+		testAssertIDValid(updated.ID)
+
+		gomega.Expect(updated.Name).Should(gomega.Equal("newName"))
+		gomega.Expect(updated.Groups).Should(gomega.HaveLen(3))
+		gomega.Expect(updated.Groups[0]).Should(gomega.Equal("first"))
+
+		var checkModel declarations.ListVariable
+		res := storage.Gorm().Table(checkModel.TableName()).Where("id = ?", updated.ID).First(&checkModel)
+		testAssertErrNil(res.Error)
+
+		gomega.Expect(checkModel.Name).Should(gomega.Equal("newName"))
+		gomega.Expect(checkModel.Groups).Should(gomega.HaveLen(3))
+		gomega.Expect(checkModel.Groups[0]).Should(gomega.Equal("first"))
+	})
+
+	ginkgo.It("should update the behaviour of the declaration variable", func() {
+		ginkgo.Skip("")
+		projectId := testCreateProject("project")
+		testCreateList(projectId, "name", 100)
+
+		m := "text value"
+		v, err := json.Marshal(m)
+		gomega.Expect(err).Should(gomega.BeNil())
+		handler := New(NewModel(projectId, []string{"name", "behaviour", "groups"}, "name", "newName", "newName", "readonly", []string{"first", "second", "third"}, []byte{}, v))
+
+		updated, err := handler.Handle()
+		testAssertErrNil(err)
+		testAssertIDValid(updated.ID)
+
+		gomega.Expect(updated.Name).Should(gomega.Equal("newName"))
+		gomega.Expect(updated.Groups).Should(gomega.HaveLen(3))
+		gomega.Expect(updated.Groups[0]).Should(gomega.Equal("first"))
+		gomega.Expect(updated.Behaviour).Should(gomega.Equal("readonly"))
+
+		var checkModel declarations.Variable
+		res := storage.Gorm().Table(checkModel.TableName()).Where("id = ?", updated.ID).First(&checkModel)
+		testAssertErrNil(res.Error)
+
+		gomega.Expect(checkModel.Name).Should(gomega.Equal("newName"))
+		gomega.Expect(checkModel.Groups).Should(gomega.HaveLen(3))
+		gomega.Expect(checkModel.Groups[0]).Should(gomega.Equal("first"))
+		gomega.Expect(checkModel.Behaviour).Should(gomega.Equal("readonly"))
+	})
+})
---
+++ pkg/app/services/lists/updateListItemByID/main.go
@@ -0,0 +1,122 @@
+package updateListItemByID
+
+import (
+	"creatif/pkg/app/domain/app"
+	"creatif/pkg/app/domain/declarations"
+	pkg "creatif/pkg/lib"
+	"creatif/pkg/lib/appErrors"
+	"creatif/pkg/lib/storage"
+	"errors"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type Main struct {
+	model Model
+}
+
+func (c Main) Validate() error {
+	if errs := c.model.Validate(); errs != nil {
+		return appErrors.NewValidationError(errs)
+	}
+
+	return nil
+}
+
+func (c Main) Authenticate() error {
+	// user check by project id should be gotten here, with authentication cookie
+	var project app.Project
+	if err := storage.Get((app.Project{}).TableName(), c.model.ProjectID, &project); err != nil {
+		return appErrors.NewAuthenticationError(err).AddError("createVariable.Authenticate", nil)
+	}
+
+	return nil
+}
+
+func (c Main) Authorize() error {
+	return nil
+}
+
+func (c Main) Logic() (declarations.ListVariable, error) {
+	var list declarations.List
+	if res := storage.Gorm().Where("name = ? AND project_id = ?", c.model.ListName, c.model.ProjectID).Select("id").First(&list); res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return declarations.ListVariable{}, appErrors.NewNotFoundError(res.Error).AddError("updateListItemByID.Logic", nil)
+		}
+
+		return declarations.ListVariable{}, appErrors.NewDatabaseError(res.Error).AddError("updateListItemByID.Logic", nil)
+	}
+
+	var existing declarations.ListVariable
+	if res := storage.Gorm().Where("id = ? AND list_id = ?", c.model.ItemID, list.ID).First(&existing); res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return declarations.ListVariable{}, appErrors.NewNotFoundError(res.Error).AddError("updateListItemByID.Logic", nil)
+		}
+
+		return declarations.ListVariable{}, appErrors.NewDatabaseError(res.Error).AddError("updateListItemByID.Logic", nil)
+	}
+
+	for _, f := range c.model.Fields {
+		if f == "name" {
+			existing.Name = c.model.Values.Name
+		}
+
+		if f == "metadata" {
+			existing.Metadata = c.model.Values.Metadata
+		}
+
+		if f == "value" {
+			existing.Value = c.model.Values.Value
+		}
+
+		if f == "groups" {
+			existing.Groups = c.model.Values.Groups
+		}
+
+		if f == "behaviour" {
+			existing.Behaviour = c.model.Values.Behaviour
+		}
+	}
+
+	var updated declarations.ListVariable
+	if res := storage.Gorm().Model(&updated).Clauses(clause.Returning{Columns: []clause.Column{
+		{Name: "id"},
+		{Name: "name"},
+		{Name: "behaviour"},
+		{Name: "metadata"},
+		{Name: "value"},
+		{Name: "groups"},
+		{Name: "created_at"},
+		{Name: "updated_at"},
+	}}).Where("id = ?", existing.ID).Select(c.model.Fields).Updates(existing); res.Error != nil {
+		return declarations.ListVariable{}, appErrors.NewApplicationError(res.Error).AddError("updateListItemByID.Logic", nil)
+	}
+
+	return updated, nil
+}
+
+func (c Main) Handle() (View, error) {
+	if err := c.Validate(); err != nil {
+		return View{}, err
+	}
+
+	if err := c.Authenticate(); err != nil {
+		return View{}, err
+	}
+
+	if err := c.Authorize(); err != nil {
+		return View{}, err
+	}
+
+	model, err := c.Logic()
+
+	if err != nil {
+		return View{}, err
+	}
+
+	return newView(model), nil
+}
+
+func New(model Model) pkg.Job[Model, View, declarations.ListVariable] {
+	return Main{model: model}
+}
